b4974baeb74660557efed5766a2120a0bb4f535a
Caio Pagliosa <caio.pinipagli@gmail.com>
2025-07-01 17:45:12 -0300

    Implementado as dependências, criando a pasta services e conexão com a api.

---
+++ lib/services/api_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String _apiKey = '5bec04789a366fa0865d913ae3b6dda5';
+  static const String _baseUrl = 'https://api.themoviedb.org/3';
+
+  Future<List<dynamic>> getPopularMovies() async {
+    final response = await http.get(Uri.parse('$_baseUrl/movie/popular?api_key=$_apiKey&language=pt-BR'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['results'];
+    } else {
+      throw Exception('Erro ao carregar filmes');
+    }
+  }
+}
